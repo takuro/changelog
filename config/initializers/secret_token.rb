261863a76c95110b9068a566116d23cd97a3015d
Takuro Ishii <isitkr@gmail.com>
2011-04-16 11:22:00 +0900

    new app 'changelog'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Changelog::Application.config.secret_token = '898472fd1630e0c9348707a269c2a5c29b5aab3ae228494eb6ffeaf9f54917b79b6a5404a59f54a1d16be043aab767e14535f039c02bd3af18b448b2eeb1e2da'
